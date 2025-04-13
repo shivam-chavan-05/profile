558cb8953b457d6661e260d58eac2c6f054fc87f
Shivam Chavan <144063863+shivam-chavan-05@users.noreply.github.com>
2025-04-13 18:19:33 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// You can add more interactive features here like dynamic content loading, smooth scroll, etc.
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Website loaded successfully!");
-  });
-  
